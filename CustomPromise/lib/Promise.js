207d544a1fde85526a3a4a849b81b44f8ad64c92
happydtd <happydtdau@yahoo.com.au>
2022-05-18 16:58:00 +1000

    add call function

--- CustomPromise/lib/Promise.js
+++ CustomPromise/lib/Promise.js
@@ -4,6 +4,7 @@
     const REJECTED = 'rejected'
     // Promise constructor
     function Promise(excutor){
+        console.log('create new promise object')
         // 将当前的promise对象保存起来
         const self = this
         self.status = PENDING //给promise对象指定status属性，初始值为pending
@@ -11,6 +12,7 @@
         self.callbacks = []  //每个元素的结构：{onResolved（）{}， onRejected（）{}}
 
         function resolve(value){
+            console.log('call resolve')
             //如果当前状态不是pending，直接结束
             if(self.status!==PENDING)
                 return
@@ -30,6 +32,7 @@
         }
 
         function reject(reason){
+            console.log('call reject')
             //如果当前状态不是pending，直接结束
             if(self.status!==PENDING)
                 return
@@ -64,7 +67,7 @@
     //返回一个新的promise对象
     Promise.prototype.then = function (onResolved, onRejected){
 
-        //指定默认的失败的回调（实现错误、异常穿透的关键点）
+        //指定默认的失败的回调（实现错误、异常穿透的关键点）,必须是函数
         onResolved = typeof onResolved==='function'? onResolved: value=>value
         onRejected = typeof onRejected==='function'? onRejected: reason=>{throw reason} 
         const self = this
@@ -95,6 +98,7 @@
 
             if (self.status === PENDING){
                 // 写入两个方法，同时修改status
+                console.log('push functions')
                 self.callbacks.push({
                     onResolved(value){ 
                         handle(onResolved);
@@ -125,19 +129,50 @@
     //promise function .resolve()
     //返回一个指定结果的成功的promise对象
     Promise.resolve = function (value){
-
+        //返回一个成功/失败的promise对象
+        return new Promise((resolve, reject)=>{
+            //value是promise
+             if(value instanceof Promise){
+                value.then(resolve, reject)
+             }else{
+                resolve(value) //value不是promise
+             }
+            
+        })
     }
 
     //promise function .reject()
    //返回一个指定reason的失败的promise对象
     Promise.reject = function (reason){
-    
+        return new Promise((resolve, reject)=>{
+            reject(reason) 
+        })
     }
 
     //promise function .all()
     //返回一个promise对象，只有当所有promise都成功时才成功，否则只要有一个失败就失败
     Promise.all = function (promises){
-        
+        // 用来保存所有成功value的数组
+        const values = new Array(promises.length)
+        // 用来保存成功promise的数量
+        let resolvedCount = 0;
+
+        return new Promise((resolve, reject)=>{
+            promises.forEach((p, index)=>{
+                p.then(
+                    value=>{
+                        resolvedCount ++
+                        //values.push(value)， 不可以用因为promise的执行速度不同会导致结果秩序不对
+                        values[index] = value
+                        if (resolvedCount === promises.length){
+                            resolve(values)
+                        }
+                    },
+                    reason=>{
+                        reject(reason)
+                    })
+            })
+        })
     }
 
     //promise function .all()
